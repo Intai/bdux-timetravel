62f54f540db4386c595c2c50cd6911b807e206d6
Intai Huang <intai.huang@vodafone.com>
2016-07-21 17:51:00 +1200

    Add web specific style for timetravel component

---
+++ src/components/timetravel-style.web.js
@@ -0,0 +1,16 @@
+export default {
+  container: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    height: '100%',
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: '2px',
+    maxWidth: '316px'
+  },
+  hideHistory: {
+    height: '111px'
+  }
+}
